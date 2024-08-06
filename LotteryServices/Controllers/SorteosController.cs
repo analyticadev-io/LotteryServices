1be51d9cbe3988de37d658c687163dcab551ba96
miguel000127@gmail.com <miguel000127@gmail.com>
2024-08-06 13:11:47 -0500

    # agregar Logica de sorteos
    dsc:
    - agregar elc ontrolador de sorteos
    - agregar Interfaz de sorteos
    - agregar Servicio de sorteos

---
+++ LotteryServices/Controllers/SorteosController.cs
@@ -0,0 +1,32 @@
+﻿using LotteryServices.Interfaces;
+using LotteryServices.Models;
+using LotteryServices.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LotteryServices.Controllers
+{
+    [Route("api/[controller]")]
+    [Authorize]
+    [ApiController]
+
+    public class SorteosController : Controller
+    {
+        private readonly ISorteo _serviceSorteo;
+
+        public SorteosController(ISorteo serviceSorteo)
+        {
+            _serviceSorteo = serviceSorteo;
+        }
+
+        //GET : /api/Sorteos
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Sorteo>>> GetSorteos()
+        {
+            var sorteos = await _serviceSorteo.GetSorteosAsync();
+            return Ok(sorteos);
+        }
+
+    }
+}
---
+++ LotteryServices/Interfaces/ISorteo.cs
@@ -0,0 +1,12 @@
+﻿using LotteryServices.Models;
+using System.Collections;
+
+namespace LotteryServices.Interfaces
+{
+    public interface ISorteo
+    {
+        Task<Sorteo> AddSorteoAsync(Sorteo sorteo);
+        Task<IEnumerable> GetSorteosAsync();
+        Task<Sorteo> GetSorteoByIdAsync(int id);
+    }
+}
