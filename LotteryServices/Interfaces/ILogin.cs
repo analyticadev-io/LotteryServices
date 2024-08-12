31052b00b69e2da475f418220b431e3ca1c55337
miguel000127@gmail.com <miguel000127@gmail.com>
2024-08-12 12:09:13 -0500

    # add autorization to User controller         dsc:         - add autorization to acces to UserController     # add AuthResponseDto to manage Auth reuqest     # add UserRolRequest to manage Users pivot roles reuqest     # update LoginAsync in interface ILogin     # update interface IRol     # update interface IRol     # update models Rol     # update service ServiceLogin     # update service ServiceRol     # update service ServiceUsuario

--- LotteryServices/Interfaces/ILogin.cs
+++ LotteryServices/Interfaces/ILogin.cs
@@ -1,11 +1,14 @@
 ﻿using LotteryServices.Models;
 using LotteryServices.Dtos;
+using Microsoft.EntityFrameworkCore;
 
 namespace LotteryServices.Interfaces
 {
     public interface ILogin
     {
         public Task RegistroAsync(Usuario usuario);
-        public Task<(bool IsSuccess, string Token)> LoginAsync(LoginDto loginDto);
+        Task<(bool IsSuccess, AuthResponseDto AuthResponse)> LoginAsync(LoginDto loginDto);     
+
+
     }
 }
--- LotteryServices/Program.cs
+++ LotteryServices/Program.cs
@@ -9,6 +9,11 @@ using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers()
+    .AddNewtonsoftJson(options =>
+        options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
