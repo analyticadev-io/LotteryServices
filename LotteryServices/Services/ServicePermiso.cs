22534cead00d0309a10671c42447971b31dbecb2
miguel000127@gmail.com <miguel000127@gmail.com>
2024-08-15 14:14:08 -0500

    [add] permiso service

---
+++ LotteryServices/Services/ServicePermiso.cs
@@ -0,0 +1,21 @@
+﻿using LotteryServices.Interfaces;
+using LotteryServices.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LotteryServices.Services
+{
+    public class ServicePermiso : IPermiso
+    {
+        private readonly LoteriaDbContext _context;
+
+        public ServicePermiso(LoteriaDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Permiso>> GetPermisosAsync()
+        {
+           return await _context.Permisos.ToListAsync();
+        }
+    }
+}
