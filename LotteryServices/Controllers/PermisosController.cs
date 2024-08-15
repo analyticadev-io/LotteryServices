099cd12edc111823ae717842b097b13ba58ad9ea
miguel000127@gmail.com <miguel000127@gmail.com>
2024-08-15 14:15:55 -0500

    [add] permiso controller

---
+++ LotteryServices/Controllers/PermisosController.cs
@@ -0,0 +1,28 @@
+﻿using LotteryServices.Interfaces;
+using LotteryServices.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LotteryServices.Controllers
+{
+    [Route("api/[controller]")]
+    [Authorize]
+    [ApiController]
+    public class PermisosController : ControllerBase
+    {
+        private readonly IPermiso _permisoService;
+
+        public PermisosController(IPermiso permisoService)
+        {
+            _permisoService = permisoService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Permiso>>> GetPermisos()
+        {
+            var usuarios = await _permisoService.GetPermisosAsync();
+            return Ok(usuarios);
+        }
+    }
+}
