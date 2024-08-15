50ad706ba10ed8a0486ff9484264217e451ed592
miguel000127@gmail.com <miguel000127@gmail.com>
2024-08-15 14:12:25 -0500

    [update] interface rol

--- LotteryServices/Interfaces/IRol.cs
+++ LotteryServices/Interfaces/IRol.cs
@@ -1,14 +1,15 @@
-﻿using LotteryServices.Models;
+﻿using LotteryServices.Dtos;
+using LotteryServices.Models;
 
 namespace LotteryServices.Interfaces
 {
     public interface IRol
     {
         Task<IEnumerable<Rol>> GetRolesAsync();
-        Task<IEnumerable<Rol>> GetRolAsync(int id);
-        Task<Rol> AddRolAsync(Rol rol);
-        Task<Rol> UpdateRolsync(Rol rol);
-        Task<bool> DeleteRolAsync(int id);
+        Task<Rol> GetRolByIdAsync(int id);
+        Task<Rol> AddRolWithPermissionsAsync(Rol rol);
+        Task<Rol> UpdateWithPermissionsRolsync(Rol rol);
+        Task<bool> DeleteRolAsync(int rolId);
         Task<bool> AsignarRolAUsuarioAsync(int usuarioId, int rolId);
         Task<bool> EditarRolAUsuarioAsync(int usuarioId, int oldRolId, int newRolId);
         Task<bool> EliminarRolAUsuarioAsync(int usuarioId,  int RolId);
