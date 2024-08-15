575e2ec11bb387dacacf51449ffdb2bdd4c149b9
miguel000127@gmail.com <miguel000127@gmail.com>
2024-08-15 14:15:22 -0500

    [add] rolPermiso Response dto

---
+++ LotteryServices/Dtos/RolPermisoResponseDto.cs
@@ -0,0 +1,10 @@
+﻿using LotteryServices.Models;
+
+namespace LotteryServices.Dtos
+{
+    public class RolPermisoResponseDto
+    {
+        public string RolName { get; set; }
+        public List<Permiso> Permisos { get; set; }
+    }
+}
