718816ed3a7dda07a4d3a47596d3a19b3675b009
miguel000127@gmail.com <miguel000127@gmail.com>
2024-08-17 21:25:59 -0500

    [add] migrations

---
+++ LotteryServices/Migrations/20240817184554_AddModulesPropoerties.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BasicBackendTemplate.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddModulesPropoerties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "icon",
+                table: "Modules",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "module_name",
+                table: "Modules",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "visibilityStatus",
+                table: "Modules",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "icon",
+                table: "Modules");
+
+            migrationBuilder.DropColumn(
+                name: "module_name",
+                table: "Modules");
+
+            migrationBuilder.DropColumn(
+                name: "visibilityStatus",
+                table: "Modules");
+        }
+    }
+}
