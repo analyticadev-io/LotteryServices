90ffef9cd9072aa1523080c5081a2aba00bf9aec
miguel000127@gmail.com <miguel000127@gmail.com>
2024-08-17 21:23:46 -0500

    [add] IModule interface

---
+++ LotteryServices/Interfaces/IModule.cs
@@ -0,0 +1,15 @@
+﻿using BasicBackendTemplate.Dtos;
+using BasicBackendTemplate.Models;
+
+namespace BasicBackendTemplate.Interfaces
+{
+    public interface IModule
+    {
+        Task<Module> AddModuleAsync(ResponseModule module);
+        Task<Module> EditModuleAsync(ResponseModule module);
+        Task <IEnumerable<Module>> GetModulesAsync();
+
+        Task<bool> DeleteModuleAsync(int module);
+
+    }
+}
