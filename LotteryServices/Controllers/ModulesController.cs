a24885389ca31ffb62ce8f71fe894a07085bbb3b
miguel000127@gmail.com <miguel000127@gmail.com>
2024-08-24 18:37:13 -0500

    [HU0002] Generate Synchronous Encryption Service for Data Transmission

--- LotteryServices/Controllers/ModulesController.cs
+++ LotteryServices/Controllers/ModulesController.cs
@@ -4,6 +4,7 @@ using LotteryServices.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace LotteryServices.Controllers
 {
@@ -13,60 +14,91 @@ namespace LotteryServices.Controllers
     public class ModulesController : ControllerBase
     {
         private readonly IModule _moduleService;
+        private readonly IEncriptado _encryptService;
 
         private readonly LoteriaDbContext _context;
 
-        public ModulesController(IModule moduleService, LoteriaDbContext context)
+        public ModulesController(IModule moduleService, LoteriaDbContext context, IEncriptado encryptService)
         {
             _moduleService = moduleService;
             _context = context;
+            _encryptService = encryptService;
         }
 
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Module>>> GetModules()
         {
             var modules = await _moduleService.GetModulesAsync();
-            return Ok(modules);
+            var jsonModules = JsonConvert.SerializeObject(modules);
+            var cryptedResponse = _encryptService.Encrypt(jsonModules);
+            var crypt = new EncryptedResponse
+            {
+                response = cryptedResponse,
+            };
+
+            return Ok(crypt);
         }
 
         [HttpPost]
-        public async Task<ActionResult<Module>> AddModule(ResponseModule module)
+        public async Task<ActionResult<Module>> AddModule(EncryptedRequest module)
         {
-            var newModule = await _moduleService.AddModuleAsync(module);
-            return Ok(newModule);
+            var decryptedRequest = _encryptService.Decrypt(module.response);
+            var objResponse = JsonConvert.DeserializeObject<ResponseModule>(decryptedRequest);
+            
+            var newModule = await _moduleService.AddModuleAsync(objResponse);
+
+            var JsonResponse = JsonConvert.SerializeObject(newModule);
+            var encryptResponse = _encryptService.Encrypt(JsonResponse);
+
+            var crypt = new EncryptedResponse
+            {
+                response = encryptResponse,
+            };
+
+            return Ok(crypt);
         }
 
         [HttpPut]
-        public async Task<ActionResult<Module>> EditModule(ResponseModule module)
+        public async Task<ActionResult<Module>> EditModule(EncryptedRequest module)
         {
-            var newModule = await _moduleService.EditModuleAsync(module);
-            return Ok(newModule);
+
+            var decryptedRequest = _encryptService.Decrypt(module.response);
+            var objResponse = JsonConvert.DeserializeObject<ResponseModule>(decryptedRequest);
+
+            var newModule = await _moduleService.EditModuleAsync(objResponse);
+            
+            var JsonResponse = JsonConvert.SerializeObject(newModule);
+            var encryptResponse = _encryptService.Encrypt(JsonResponse);
+
+            var crypt = new EncryptedResponse
+            {
+                response = encryptResponse,
+            };
+
+            return Ok(crypt);
         }
 
         [HttpDelete("{module}")]
-        public async Task<ActionResult> DeleteModule(int module)
+        public async Task<ActionResult> DeleteModule(string module)
         {
-            try
-            {
-                bool isDeleted = await _moduleService.DeleteModuleAsync(module);
+            var decryptedRequest = _encryptService.Decrypt(module);
+            
+           
 
-                if (isDeleted)
-                {
-                    return Ok(new { message = "El módulo y sus permisos han sido eliminados con éxito." });
-                }
-                else
-                {
-                    return BadRequest(new { message = "Hubo un problema al eliminar el módulo." });
-                }
-            }
-            catch (InvalidOperationException ex)
+            bool isDeleted = await _moduleService.DeleteModuleAsync(Convert.ToInt32(decryptedRequest));
+
+            if (isDeleted)
             {
-                return NotFound(new { message = ex.Message });
+                return Ok(new EncryptedResponse { response=_encryptService.Encrypt("OK")});
             }
-            catch (Exception ex)
+            else
             {
-                return StatusCode(500, new { message = "Ocurrió un error al procesar la solicitud.", details = ex.Message });
+                return BadRequest(new { message = "Hubo un problema al eliminar el módulo." });
             }
+
+
+
+
         }
 
 
